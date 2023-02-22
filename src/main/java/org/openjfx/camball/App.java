f7c88069239c1185965c3a5cdcc959bfdba514cf
sunniestAcorn00 <30845397+cameodud234@users.noreply.github.com>
2023-02-22 02:08:20 -0500

    Seperated game logic from main.

--- src/main/java/org/openjfx/camball/App.java
+++ src/main/java/org/openjfx/camball/App.java
@@ -24,7 +24,6 @@ public class App extends Application {
 		
 		final int widthX = 800;
 		final int widthY = 800;
-		
 	  
 		Group root = new Group();
    	  
@@ -33,76 +32,15 @@ public class App extends Application {
 		ToolBar toolbar = new ToolBar();
 		
 		root.getChildren().add(toolbar);
+		
+		GameLogic game = new GameLogic(widthX, widthY);
    
-		// your game logic goes here  
 		AnimationTimer timer = new AnimationTimer() {
-			double pixelY = 100;
-			boolean movingUp = false;
-			boolean movingDown = true;
-			
-			double pixelX = 100;
-			boolean movingLeft = false;
-			boolean movingRight = true;
-			
-			double deltaX = 3;
-			double deltaY = 5;
-			
-			Random rand = new Random();
-			
 			@Override
 			public void handle(long now) {
-			  
-				root.getChildren().clear();
-			  
-				Circle circle = new Circle(pixelX, pixelY, 20);
-				
-				circle.setFill(Color.BLUE);
-			  
-//				root.getChildren().remove(circle);
-			  
-				if(circle.getCenterY() + circle.getRadius() < widthY - 1 && movingDown) {
-					movingUp = false;
-					movingDown = true;
-					pixelY += deltaY;
-					
-				}
-				else {
-					
-					movingUp = true;
-					movingDown = false;
-					pixelY -= deltaY;
-					
-					if(circle.getCenterY() - circle.getRadius() <= 0) {
-						movingUp = false;
-						movingDown = true;
-					}
-					
-				}
-				
-				if(circle.getCenterX() + circle.getRadius() < widthX - 1 && movingRight) {
-					movingLeft = false;
-					movingRight = true;
-					pixelX += deltaX;
-				}
-				else {
-					movingLeft = true;
-					movingRight = false;
-					pixelX -= deltaX;
-					
-					if(circle.getCenterX() - circle.getRadius() <= 0) {
-						movingLeft = false;
-						movingRight = true;
-					}
-					
-				}
-
-				
-			  
-				root.getChildren().add(circle);
-            
-			}
-    
-	  
+				game.update();
+				root.getChildren().setAll(game.getCircle());
+			}    
 		};
 	  
 		timer.start();
---
+++ src/main/java/org/openjfx/camball/GameLogic.java
@@ -0,0 +1,84 @@
+package org.openjfx.camball;
+
+import javafx.scene.paint.Color;
+import javafx.scene.shape.Circle;
+
+public class GameLogic {
+	
+	private final int widthX;
+	private final int widthY;
+	
+	private double pixelY;
+	private boolean movingUp;
+	private boolean movingDown;
+	
+	private double pixelX;
+	private boolean movingLeft;
+	private boolean movingRight;
+	
+	private double deltaX;
+	private double deltaY;
+	
+	private Circle circle;
+	
+	public GameLogic(int widthX, int widthY) {
+		this.widthX = widthX;
+		this.widthY = widthY;
+		
+		pixelY = 100;
+		movingUp = false;
+		movingDown = true;
+		
+		pixelX = 100;
+		movingLeft = false;
+		movingRight = true;
+		
+		deltaX = 3;
+		deltaY = 5;
+		
+		circle = new Circle(pixelX, pixelY, 20);
+		
+		circle.setFill(Color.BLUE);
+
+	}
+	
+	public void update() {
+		
+		if(circle.getCenterY() + circle.getRadius() < widthY - 1 && movingDown) {
+            movingUp = false;
+            movingDown = true;
+            pixelY += deltaY;
+        } else {
+            movingUp = true;
+            movingDown = false;
+            pixelY -= deltaY;
+            if(circle.getCenterY() - circle.getRadius() <= 0) {
+                movingUp = false;
+                movingDown = true;
+            }
+        }
+        
+        if(circle.getCenterX() + circle.getRadius() < widthX - 1 && movingRight) {
+            movingLeft = false;
+            movingRight = true;
+            pixelX += deltaX;
+        } else {
+            movingLeft = true;
+            movingRight = false;
+            pixelX -= deltaX;
+            if(circle.getCenterX() - circle.getRadius() <= 0) {
+                movingLeft = false;
+                movingRight = true;
+            }
+        }
+		
+		circle.setCenterX(pixelX);
+		circle.setCenterY(pixelY);
+		
+	}
+	
+	public Circle getCircle() {
+		return circle;
+	}
+	
+}
