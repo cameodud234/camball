708a1a11e2b6a32e3c8de46d4ec674de25ddd004
sunniestAcorn00 <30845397+cameodud234@users.noreply.github.com>
2023-05-03 21:11:08 -0400

    Update

--- src/main/java/org/openjfx/camball/BallSimulation.java
+++ src/main/java/org/openjfx/camball/BallSimulation.java
@@ -52,7 +52,7 @@ public class BallSimulation extends AnimationTimer {
 		
 		ballStates = new HashSet<>();
 	
-		BallState ballState1 = new BallState(new Velocity(20, -20), new Position(400, physics.getScreenHeight()/2), 
+		BallState ballState1 = new BallState(new Velocity(20, 0), new Position(400, physics.getScreenHeight()/2), 
 				200, Color.ANTIQUEWHITE, 5, physics);
 		
 		BallState ballState2 = new BallState(new Velocity(-20, 0), new Position(physics.getScreenWidth() - 200, 
